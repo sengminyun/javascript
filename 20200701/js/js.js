b19f8dadb09c682fda7619e32f42d7a7fc9266f9
eunnyyy <jse1282@gmail.com>
2020-07-01 12:09:48 +0900

    js

---
+++ 20200701/js/js.js
@@ -0,0 +1,17 @@
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+// document.getElementById('id');
+// document.getElementsByClassName('box');
+// document.getElementsByTagName('p');
+
+
+
+// var num=document.getElementById('id');
+
+// document.getElementById('id').innerHTML=2*5;
+// num.innerHTML=2*5;
+
+document.querySelector('#id').innerHTML=10*10;
+document.querySelector('.box').innerHTML=20*20;
